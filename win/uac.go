4bea3578383776888df71622791e43e51af7a844
Node <8974108+qwenode@users.noreply.github.com>
2021-09-26 10:49:09 +0800

    add win.RequireUAC()

--- win/uac.go
+++ win/uac.go
@@ -8,7 +8,7 @@ import (
 )
 
 //Request UAC elevation in Go,要求以管理员身份运行程序
-func RequireUAC() error {
+func RequireUAC() bool {
 	_, err := os.Open("\\\\.\\PHYSICALDRIVE0")
 	if err != nil {
 		verb := "runas"
@@ -24,7 +24,10 @@ func RequireUAC() error {
 		var showCmd int32 = 1 //SW_NORMAL
 
 		err := windows.ShellExecute(0, verbPtr, exePtr, argPtr, cwdPtr, showCmd)
-		return err
+		if err != nil {
+			return false
+		}
+		return true
 	}
-	return nil
+	return true
 }
