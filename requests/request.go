8d85d45f4e2e2b71b6f1240c1cb5e8f0120dc2d4
Node <8974108+qwenode@users.noreply.github.com>
2021-10-04 14:27:52 +0800

    remove win packge remove request.download

--- requests/request.go
+++ requests/request.go
@@ -3,10 +3,8 @@ package requests
 import (
 	"crypto/tls"
 	"github.com/qwenode/gogo/convert"
-	"io"
 	"net"
 	"net/http"
-	"os"
 	"time"
 )
 
@@ -31,67 +29,4 @@ func GetHeaderSize(url string) int64 {
 	}
 	defer head.Body.Close()
 	return convert.ToInt64(head.Header.Get("Content-Length"))
-}
-
-// Download save http response body to file,download large file
-func Download(url string, toFile string) (int64, error) {
-	create, err := os.Create(toFile)
-	if err != nil {
-		return 0, err
-	}
-	defer create.Close()
-	get, err := http.Get(url)
-	if err != nil {
-		return 0, err
-	}
-	defer get.Body.Close()
-	written, _ := io.Copy(create, get.Body)
-	return written, nil
-}
-
-// downloadFunc DownloadFn callback
-type downloadFunc func(nowSize int64)
-
-// DownloadFn save http response body to file,download large file
-func DownloadFn(fn downloadFunc, url string, toFile string) (int64, error) {
-	dst, err := os.Create(toFile)
-	if err != nil {
-		return 0, err
-	}
-	defer dst.Close()
-	get, err := http.Get(url)
-	if err != nil {
-		return 0, err
-	}
-	defer get.Body.Close()
-	buf := make([]byte, 32*1024)
-	written := int64(0)
-	var err2 error
-	for {
-		nr, er := get.Body.Read(buf)
-		if nr > 0 {
-			nw, ew := dst.Write(buf[0:nr])
-			if nw > 0 {
-				written += int64(nw)
-				fn(written)
-			}
-			if ew != nil {
-				err2 = ew
-				break
-			}
-			if nr != nw {
-				err2 = io.ErrShortWrite
-				break
-			}
-		}
-		if er != nil {
-			if er != io.EOF {
-				err2 = er
-			}
-			break
-		}
-	}
-	//written, _ := io.Copy(create, get.Body)
-	//get.Body.Read()
-	return written, err2
 }
--- requests/request_test.go
+++ requests/request_test.go
@@ -1,68 +1,9 @@
 package requests
 
 import (
-	"github.com/qwenode/gogo/convert"
-	"log"
-	"os"
 	"testing"
 )
 
-func TestDownload(t *testing.T) {
-	type args struct {
-		url    string
-		toFile string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    int64
-		wantErr bool
-	}{
-		{
-			args: args{
-				url:    "https://www.ipip.net",
-				toFile: "./download_testcase",
-			},
-			want: 1,
-		},
-		{
-			args: args{
-				url:    "https://www.baidu.com",
-				toFile: "./download_testcase",
-			},
-			want: 1,
-		},
-		{
-			args: args{
-				url:    "https://www.ipip.net",
-				toFile: "/dev/test",
-			},
-			want:    0,
-			wantErr: true,
-		},
-		{
-			args: args{
-				url:    "http://www.ipip.netx:99",
-				toFile: "./download_testcase",
-			},
-			want:    0,
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := Download(tt.args.url, tt.args.toFile)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("Download() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got < tt.want {
-				t.Errorf("Download() got = %v, want > %v", got, tt.want)
-			}
-		})
-	}
-	os.Remove("./download_testcase")
-}
 
 func TestGetHeaderSize(t *testing.T) {
 	type args struct {
@@ -89,65 +30,4 @@ func TestGetHeaderSize(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestDownloadFn(t *testing.T) {
-	type args struct {
-		fn     downloadFunc
-		url    string
-		toFile string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    int64
-		wantErr bool
-	}{
-		{
-			args: args{
-				fn: func(nowSize int64) {
-					log.Println("download size:" + convert.ToString(nowSize))
-				},
-				url:    "http://ecms.phome.net/downcenter/empirecms/ecms75/tool/ecms_memberconnect_qq.zip",
-				toFile: "./download_testcase",
-			},
-			want:    1,
-			wantErr: false,
-		},
-		{
-			args: args{
-				fn: func(nowSize int64) {
-					log.Println("download size:" + convert.ToString(nowSize))
-				},
-				url:    "http://ecms.phome/wewf",
-				toFile: "./download_testcase",
-			},
-			want:    0,
-			wantErr: true,
-		},
-		{
-			args: args{
-				fn: func(nowSize int64) {
-					log.Println("download size:" + convert.ToString(nowSize))
-				},
-				url:    "http://ecms.phome/wewf",
-				toFile: "./",
-			},
-			want:    0,
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := DownloadFn(tt.args.fn, tt.args.url, tt.args.toFile)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("DownloadFn() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if got < tt.want {
-				t.Errorf("DownloadFn() got = %v, want %v", got, tt.want)
-			}
-		})
-	}
-	os.Remove("./download_testcase")
 }
