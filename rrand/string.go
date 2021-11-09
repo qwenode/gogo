dc0325bae60cf7042b9223026d58358af5f5b279
Node <8974108+qwenode@users.noreply.github.com>
2021-11-09 15:45:08 +0800

    add GetRandNumber fix GetRandStringNormal

--- rrand/string.go
+++ rrand/string.go
@@ -5,6 +5,8 @@ import (
 	"encoding/binary"
 	"fmt"
 	math_rand "math/rand"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -17,14 +19,22 @@ func GetRandString(n int) string {
 
 // GetRandStringNormal get rrand string use own seeds
 func GetRandStringNormal(n int) string {
-	b := make([]byte, n)
-	var runes = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
-	for i := range b {
-		b[i] = runes[math_rand.Intn(len(runes))]
+	b := ""
+	var runes = strings.Split("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", "")
+	for i := 0; i < n; i++ {
+		b += runes[math_rand.Intn(len(runes))]
 	}
-	return fmt.Sprintf("%x", b)[:n]
+	return b
 }
-
+func GetRandNumber(length int) string {
+	b := ""
+	runes := Perm(10)
+	for i := 0; i < length; i++ {
+		r := math_rand.Intn(len(runes))
+		b += strconv.Itoa(runes[r])
+	}
+	return b
+}
 func init() {
 	var rb [4]byte
 	_, err := crypto_rand.Read(rb[:])
--- rrand/string_test.go
+++ rrand/string_test.go
@@ -55,3 +55,30 @@ func TestGetRandStringNormal(t *testing.T) {
 		})
 	}
 }
+
+func TestGetRandNumber(t *testing.T) {
+	type args struct {
+		length int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			args: args{length: 1},
+			want: 1,
+		},
+		{
+			args: args{length: 6},
+			want: 6,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := GetRandNumber(tt.args.length); len(got) != tt.want {
+				t.Errorf("GetRandNumber() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
