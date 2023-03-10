676e43ea967112039440ec937f8fcfd73a7956df
Node <8974108+qwenode@users.noreply.github.com>
2023-03-10 17:30:52 +0800

    structs.ToMapByTag structs.ToMapByTagJson

--- sstruct/struct.go
+++ sstruct/struct.go
@@ -6,9 +6,12 @@ import (
 
 // ToFieldValueMap struct to map
 func ToFieldValueMap(data interface{}) map[string]interface{} {
+    list := map[string]interface{}{}
+    if data == nil {
+        return list
+    }
     v := reflect.ValueOf(data)
     typeV := v.Type()
-    list := map[string]interface{}{}
     for i := 0; i < v.NumField(); i++ {
         list[typeV.Field(i).Name] = v.Field(i).Interface()
     }
@@ -19,9 +22,13 @@ func ToFieldValueMap(data interface{}) map[string]interface{} {
 
 // ToMapByTag  struct to map using tag as key
 func ToMapByTag(data interface{}, tag string) map[string]interface{} {
+    list := map[string]interface{}{}
+    if data == nil {
+        return list
+    }
     v := reflect.ValueOf(data)
     typeV := v.Type()
-    list := map[string]interface{}{}
+    
     for i := 0; i < v.NumField(); i++ {
         fieldTag, ok := typeV.Field(i).Tag.Lookup(tag)
         if !ok {
