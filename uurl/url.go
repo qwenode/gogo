bc3be49773f8db44b97e176185632332b113da54
Node <8974108+qwenode@users.noreply.github.com>
2023-04-05 21:44:52 +0800

    uurl.IsUrl

--- uurl/url.go
+++ uurl/url.go
@@ -4,6 +4,15 @@ import "net/url"
 
 // UrlDecode url.QueryUnescape
 func UrlDecode(s string) string {
-	unescape, _ := url.QueryUnescape(s)
-	return unescape
+    unescape, _ := url.QueryUnescape(s)
+    return unescape
+}
+
+// IsUrl check url
+func IsUrl(s string) bool {
+    _, err := url.Parse(s)
+    if err != nil {
+        return false
+    }
+    return true
 }
