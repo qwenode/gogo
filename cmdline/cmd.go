f6a36849ef237dfa1a4d0ef6050d68aeb9fdbe81
Node <8974108+qwenode@users.noreply.github.com>
2024-04-11 20:51:20 +0800

    reformat cmdline.CommandRealtimeStdout

--- cmdline/cmd.go
+++ cmdline/cmd.go
@@ -1,14 +1,16 @@
 package cmdline
 
 import (
+	"bufio"
 	"github.com/qwenode/gogo/sanitize"
+	"io/ioutil"
 	"os/exec"
 )
 
-//windows command: cmdline.exe /c "command"
-//linux command: /bin/bash -c "command"
-//TODO add CommandRealtimeStdoutTimeout run with timeout
-//TODO add CommandFnTimeout run with timeout
+// windows command: cmdline.exe /c "command"
+// linux command: /bin/bash -c "command"
+// TODO add CommandRealtimeStdoutTimeout run with timeout
+// TODO add CommandFnTimeout run with timeout
 // commandFunc call by CommandFn
 type commandFunc func(output string, errCode int) bool
 
@@ -19,4 +21,35 @@ func CommandFn(fn commandFunc, name string, arg ...string) bool {
 		return fn(string(output), sanitize.Int(err.Error()))
 	}
 	return fn(string(output), 0)
+}
+
+// commandStdoutFunc call by CommandRealtimeStdout
+type commandStdoutFunc func(output string, errCode int, done bool) bool
+
+// CommandRealtimeStdout run exec.command(name,arg...) and realtime output
+func CommandRealtimeStdout(fn commandStdoutFunc, name string, arg ...string) bool {
+	command := exec.Command(name, arg...)
+	pipe, _ := command.StdoutPipe()
+	stderrPipe, _ := command.StderrPipe()
+	defer pipe.Close()
+	defer stderrPipe.Close()
+	if err := command.Start(); err != nil {
+		all, _ := ioutil.ReadAll(stderrPipe)
+		return fn(string(all), sanitize.Int(string(all)), true)
+	}
+	reader := bufio.NewReader(pipe)
+	r := true
+	for {
+		readString, err := reader.ReadString('\n')
+		if err != nil {
+			all, _ := ioutil.ReadAll(stderrPipe)
+			r = fn(string(all), sanitize.Int(string(all)), true)
+			break
+		}
+		r = fn(readString, 0, false)
+		if r == false {
+			break
+		}
+	}
+	return r
 }
--- cmdline/cmd_linux.go
+++ cmdline/cmd_linux.go
@@ -1,43 +1,5 @@
 package cmdline
 
-import (
-	"bufio"
-	"github.com/qwenode/gogo/sanitize"
-	"io/ioutil"
-	"os/exec"
-)
-
-// commandStdoutFunc call by CommandRealtimeStdout
-type commandStdoutFunc func(output string, errCode int, done bool) bool
-
-// CommandRealtimeStdout run exec.command(name,arg...) and realtime output
-func CommandRealtimeStdout(fn commandStdoutFunc, name string, arg ...string) bool {
-	command := exec.Command(name, arg...)
-	pipe, _ := command.StdoutPipe()
-	stderrPipe, _ := command.StderrPipe()
-	defer pipe.Close()
-	defer stderrPipe.Close()
-	if err := command.Start(); err != nil {
-		all, _ := ioutil.ReadAll(stderrPipe)
-		return fn(string(all), sanitize.Int(string(all)), true)
-	}
-	reader := bufio.NewReader(pipe)
-	r := true
-	for {
-		readString, err := reader.ReadString('\n')
-		if err != nil {
-			all, _ := ioutil.ReadAll(stderrPipe)
-			r = fn(string(all), sanitize.Int(string(all)), true)
-			break
-		}
-		r = fn(readString, 0, false)
-		if r == false {
-			break
-		}
-	}
-	return r
-}
-
 // CommandLineRealtimeStdout run exec.command(name,arg...) and realtime output ,just one command,actually exec /bin/bash -c "your command"
 func CommandLineRealtimeStdout(fn commandStdoutFunc, c string) bool {
 	return CommandRealtimeStdout(fn, "/bin/bash", "-c", c)
