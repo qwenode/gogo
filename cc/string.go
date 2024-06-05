e8d56eb38acabf84be4f6e29d54a4b5796b5c81c
Node <8974108+qwenode@users.noreply.github.com>
2024-06-05 16:29:28 +0800

    cc.ToBoolean

--- cc/string.go
+++ cc/string.go
@@ -1,26 +1,52 @@
 package cc
 
 import (
-	"fmt"
-	"strconv"
+    "fmt"
+    "strconv"
+    "strings"
 )
 
 // ToString convert to string
 func ToString(str interface{}) string {
-	switch str.(type) {
-	case int:
-		return strconv.Itoa(str.(int))
-	case int32:
-		return fmt.Sprintf("%v", str.(int32))
-	case int64:
-		return fmt.Sprintf("%v", str.(int64))
-	case string:
-		return str.(string)
-	case float64:
-		return fmt.Sprintf("%v", str.(float64))
-	case float32:
-		return fmt.Sprintf("%v", str.(float32))
-	default:
-		return ""
-	}
+    switch str.(type) {
+    case int:
+        return strconv.Itoa(str.(int))
+    case int32:
+        return fmt.Sprintf("%v", str.(int32))
+    case int64:
+        return fmt.Sprintf("%v", str.(int64))
+    case string:
+        return str.(string)
+    case float64:
+        return fmt.Sprintf("%v", str.(float64))
+    case float32:
+        return fmt.Sprintf("%v", str.(float32))
+    default:
+        return ""
+    }
+}
+
+// ToBoolean convert to boolean
+func ToBoolean(data interface{}) bool {
+    switch data.(type) {
+    case int:
+    case int32:
+    case int64:
+    case uint:
+    case uint32:
+    case uint64:
+    case uint8:
+    case int8:
+        toInt := ToInt(data)
+        return toInt > 0
+    case string:
+        s := strings.TrimSpace(strings.ToLower(data.(string)))
+        if s == "1" || s == "ok" || s == "true" || s == "y" || s == "yes" || s == "t" {
+            return true
+        }
+        return false
+    case bool:
+        return data.(bool)
+    }
+    return false
 }
